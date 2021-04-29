8edda63bf2e728b0ab9bc631ce01ef4f526d69b2
jjwon915 <jjwon915@naver.com>
2021-04-29 16:06:13 +0900

    MVC 방식

--- spring-practice/spring-practice/src/main/java/helloSpring/springpractice/controller/WelcomeController.java
+++ spring-practice/spring-practice/src/main/java/helloSpring/springpractice/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package helloSpring.springpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WelcomeController {
@@ -11,5 +12,14 @@ public class WelcomeController {
         model.addAttribute("data1", "Welcome To my Page! Hello!");
         model.addAttribute("data2", "addAttribute Test");
         return "hello";
+    }
+
+    @GetMapping("hi")
+    public String helloMvc(@RequestParam(value = "name", defaultValue = "hong gil-dong") String name,
+                           @RequestParam(value = "age", defaultValue = "99") String age,
+                           Model model){
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "info";
     }
 }
